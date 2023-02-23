a2f19400e78a5e549024bfa6889f386bcf3228b1
Leandro Gabriel Gor <90048504+leandrogor@users.noreply.github.com>
2023-02-22 21:13:32 -0300

    big start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'inicio', pathMatch: 'full' },
+
+      {path: 'home', component: HomeComponent },
+      {path: 'wcm', component: WelcomeComponent },
+      
+      // {path: '**', component: PaginaNoEncontradaComponent }
+      {path: '**', redirectTo: 'inicio' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
